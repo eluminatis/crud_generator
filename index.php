11ad4934cde6e2662fbd06ea817f69ff7496884e
Peterson Passos <peterson.jfp@gmail.com>
2018-03-08 20:06:23 -0300

    adicionando select de fw

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     </head>
     <style>
         * {
-          font-family: sans-serif;
+            font-family: sans-serif;
         }
     </style>
     <body style="background: #cccccc">
         <div style="width: 60%; margin: 15px auto;">
             <h1>Projeto ajudante</h1>
+
             <h2>
                 Adicione o nome da classe e a lista de atributos dela no formulário abaixo e clique em enviar
             </h2>
@@ -22,6 +23,14 @@
                 <label for="atributos" style="font-size: 18pt;">Adicione os atributos da classe separados por virgula ','</label><br><br>
                 <textarea rows="10" name="atributos" style="font-size: 18pt; background-color: #000; color:#fff; width: 100%"></textarea><br><br>
 
+                <label for="selecet_fw">Selecione o framework</label>
+                <select id="selecet_fw" name="selecet_fw" class="form-control">
+                    <option>Laravel</option>
+                    <option>Code Igniter</option>
+                    <option>PHP puro</option>
+                    <option>Synphony</option>
+                </select>
+                <br /><br />
                 <button type="submit" style="width: 250px; height: 100px;font-size: 18pt;">Enviar</button>
             </form>
         </div>
